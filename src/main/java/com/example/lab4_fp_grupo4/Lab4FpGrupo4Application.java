f0f7f2d0efd5732a11828da9386b0a42998a5af3
Leonardo Ayala <leonardo.ayalao@pucp.edu.pe>
2022-04-18 10:51:06 -0500

    mi primer commit

---
+++ src/main/java/com/example/lab4_fp_grupo4/Lab4FpGrupo4Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_fp_grupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4FpGrupo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4FpGrupo4Application.class, args);
+    }
+
+}
